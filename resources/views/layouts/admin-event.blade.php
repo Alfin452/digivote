82b32c42515b083f75a0910a8aff7e247f1ee9e4
Nosezzy <151822668+Alfin452@users.noreply.github.com>
2026-03-09 08:56:38 +0800

    QR CODE (ADMIN EVENT)

--- resources/views/layouts/admin-event.blade.php
+++ resources/views/layouts/admin-event.blade.php
@@ -42,7 +42,7 @@
                 </svg>
                 Data Suara
             </a>
-            <a href="#" class="flex items-center px-3 py-2.5 text-gray-700 hover:bg-gray-100 rounded-lg font-medium transition-colors">
+            <a href="{{ route('admin-event.qr-links') }}" class="flex items-center px-3 py-2.5 text-gray-700 hover:bg-gray-100 rounded-lg font-medium transition-colors">
                 <svg class="w-5 h-5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v1m6 11h2m-6 0h-2v4m0-11v3m0 0h.01M12 12h4.01M16 20h4M4 12h4m12 0h.01M5 8h2a1 1 0 001-1V5a1 1 0 00-1-1H5a1 1 0 00-1 1v2a1 1 0 001 1zm12 0h2a1 1 0 001-1V5a1 1 0 00-1-1h-2a1 1 0 00-1 1v2a1 1 0 001 1zM5 20h2a1 1 0 001-1v-2a1 1 0 00-1-1H5a1 1 0 00-1 1v2a1 1 0 001 1z"></path>
                 </svg>
